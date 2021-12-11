328c5ec933068acd37c1346bb1e077fffb24e88e
Zdravko Hristov <zdravkohristov0@gmail.com>
2021-12-11 10:42:37 +0200

    added controls to partners slider

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,10 @@
 import TouchSwiper from './touch-swiper.js';
 
-new TouchSwiper('.touch-slider');
+new TouchSwiper('.touch-slider', {
+  prevSelector: '.partners .prev',
+  nextSelector: '.partners .next',
+});
+
 new TouchSwiper('.portfolio-slider', {
   prevSelector: '.portfolio .prev',
   nextSelector: '.portfolio .next',
